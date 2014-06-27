7a2e9eb1147a22f475c44fdbc164da9cd741e03e
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2014-06-27 21:06:37 +0200

    Technical: Fixed release cmd

--- DeployToNuget.cmd
+++ DeployToNuget.cmd
@@ -1,7 +1,7 @@
 REM ON FIRST RUN, RUN THIS (change the key to whatever is found on your profile on www.nuget.org ->   
 REM .nuget\NuGet.exe setapikey e39ea-get-the-full-key-on-nuget.org
 
-CreateNuget.cmd
+call CreateNuget.cmd
 .nuget\NuGet.exe push nuget_packages\StatePrinter.1.0.?.nupkg
 
 pause 
