ad9892fa9a5076683d8fef3799f0a37ae3b4e92f
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2015-01-17 18:34:00 +0100

    Technical: Added stylecop

--- StatePrinter.Tests/IntegrationTests/CultureTests.cs
+++ StatePrinter.Tests/IntegrationTests/CultureTests.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿// Copyright 2014-2015 Kasper B. Graversen
+// 
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+using System;
 using System.Globalization;
 using NUnit.Framework;
 using StatePrinter.Configurations;
@@ -8,7 +27,7 @@ namespace StatePrinter.Tests.IntegrationTests
   [TestFixture]
   class CultureTests
   {
-    const decimal decimalNumber = 12345.343M;
+    const decimal DecimalNumber = 12345.343M;
     readonly DateTime dateTime = new DateTime(2010, 2, 28, 22, 10, 59);
     
     [Test]
@@ -18,8 +37,8 @@ namespace StatePrinter.Tests.IntegrationTests
       cfg.Culture = new CultureInfo("en-US");
       var usPrinter = new Stateprinter(cfg);
 
-      Assert.AreEqual("12345.343\r\n", usPrinter.PrintObject(decimalNumber));
-      Assert.AreEqual("12345.34\r\n", usPrinter.PrintObject((float)decimalNumber));
+      Assert.AreEqual("12345.343\r\n", usPrinter.PrintObject(DecimalNumber));
+      Assert.AreEqual("12345.34\r\n", usPrinter.PrintObject((float)DecimalNumber));
       Assert.AreEqual("2/28/2010 10:10:59 PM\r\n", usPrinter.PrintObject(dateTime));
     }
 
@@ -30,8 +49,8 @@ namespace StatePrinter.Tests.IntegrationTests
       cfg.Culture = new CultureInfo("da-DK");
       var dkPrinter = new Stateprinter(cfg);
 
-      Assert.AreEqual("12345,343\r\n", dkPrinter.PrintObject(decimalNumber));
-      Assert.AreEqual("12345,34\r\n", dkPrinter.PrintObject((float)decimalNumber));
+      Assert.AreEqual("12345,343\r\n", dkPrinter.PrintObject(DecimalNumber));
+      Assert.AreEqual("12345,34\r\n", dkPrinter.PrintObject((float)DecimalNumber));
       Assert.AreEqual("28-02-2010 22:10:59\r\n", dkPrinter.PrintObject(dateTime));
     }
   }
